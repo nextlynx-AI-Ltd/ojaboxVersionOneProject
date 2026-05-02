6505302e5ef2ed9641d8098a71703844902e606a
Timmy <timilehinamolegbe1@gmail.com>
2026-05-03 00:35:02 +0100

    fix: next.config

--- next.config.ts
+++ next.config.ts
@@ -10,8 +10,8 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.paystack.co https://checkout.paystack.com",
               "style-src 'self' 'unsafe-inline' https://checkout.paystack.com https://paystack.com",
-              "style-src 'self' 'unsafe-inline' https://checkout.paystack.com",
               "frame-src 'self' https://checkout.paystack.com",
               "img-src 'self' data: https://checkout.paystack.com",
               "connect-src 'self' https://api.paystack.co https://checkout.paystack.com",
